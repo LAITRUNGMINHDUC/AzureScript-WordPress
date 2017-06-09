5ce3504091d50c1c366de3fc0e2dc9a809b5210a
Duc Lai Trung Minh (Henry Lai) <MINHDUC1996@users.noreply.github.com>
2017-06-09 23:14:31 +0700

    Quick install WordPress with MySQL in-app

---
+++ SetupWP.bat
@@ -0,0 +1,8 @@
+cd D:\home\site\wwwroot
+curl https://wordpress.org/latest.zip > WP.zip
+unzip WP.zip
+cd wordpress
+curl https://raw.github.com/MINHDUC1996/QuickInstall-Azure-WordPress/master/wp-config.php
+cls
+echo "Install latest WordPress with MySQL in-app successfully"
+exit
