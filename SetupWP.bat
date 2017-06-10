47ba0f39629ccb97358e6a63ed94dfce12efa9bd
Duc Lai Trung Minh (Henry Lai) <MINHDUC1996@users.noreply.github.com>
2017-06-10 14:44:47 +0700

    Update CURL Github parameter

--- SetupWP.bat
+++ SetupWP.bat
@@ -2,7 +2,7 @@ cd D:\home\site\wwwroot
 curl https://wordpress.org/latest.zip > WP.zip
 unzip WP.zip
 cd wordpress
-curl -O "https://raw.github.com/MINHDUC1996/QuickInstall-Azure-WordPress/master/wp-config.php"
+curl -L -O "https://raw.github.com/MINHDUC1996/QuickInstall-Azure-WordPress/master/wp-config.php"
 cls
 echo "Install latest WordPress with MySQL in-app successfully"
 exit
