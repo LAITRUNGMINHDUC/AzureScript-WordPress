b3ffbccbbeafdb886867e8817f775ef4b932bfc8
Duc Lai Trung Minh (Henry Lai) <MINHDUC1996@users.noreply.github.com>
2017-06-18 11:23:42 +0700

    Update SetupWP.bat

--- SetupWP.bat
+++ SetupWP.bat
@@ -6,4 +6,3 @@ curl -L -O "https://raw.github.com/MINHDUC1996/AzureScript-WordPress/master/wp-c
 curl -L -O "https://raw.github.com/MINHDUC1996/AzureScript-WordPress/master/web.config"
 cd D:\home\site\wwwroot
 rm WP.zip
-rm SetupWP.bat
